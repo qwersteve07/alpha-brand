5fb8418cfdca77e70468070e49f1df2fd491aa16
qwersteve07 <qwersteve07@gmail.com>
2022-03-07 01:33:54 +0800

    test

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 echo 'start to sync to server...'
 
-rsync -avhzr -e 'ssh -p 18765 -o UserKnownHostsFile=./ssh_config' ./out/ u290-kv7ntqfmf0cj@alphabrand.tw:/home/u290-kv7ntqfmf0cj/www/alphabrand.tw/public_html/
+rsync -avhzr -e 'ssh -p 18765 -o UserKnownHostsFile=./ssh_config StrictHostKeyChecking=no' ./out/ u290-kv7ntqfmf0cj@alphabrand.tw:/home/u290-kv7ntqfmf0cj/www/alphabrand.tw/public_html/
 
 echo 'sync complete!'
